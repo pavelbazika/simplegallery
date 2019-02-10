a6b6a6ed2e083d926d679db1a55ae7cc423dffa3
Pavel Bazika <pavel.bazika@icewarp.com>
2019-02-10 16:46:38 +0100

    Lint fixes

--- simplegallery/src/main/java/it/feio/android/simplegallery/views/TouchImageView.java
+++ simplegallery/src/main/java/it/feio/android/simplegallery/views/TouchImageView.java
@@ -39,7 +39,7 @@ import android.widget.ImageView;
 import android.widget.OverScroller;
 import android.widget.Scroller;
 
-public class TouchImageView extends ImageView {
+public class TouchImageView extends android.support.v7.widget.AppCompatImageView {
 	
 	private static final String DEBUG = "DEBUG";
 	
@@ -404,7 +404,7 @@ public class TouchImageView extends ImageView {
     /**
      * Set zoom parameters equal to another TouchImageView. Including scale, position,
      * and ScaleType.
-     * @param TouchImageView
+     * @param img
      */
     public void setZoom(TouchImageView img) {
     	PointF center = img.getScrollPosition();
@@ -741,7 +741,7 @@ public class TouchImageView extends ImageView {
     	} else if (Math.abs(x) + viewWidth + 1 >= getImageWidth() && direction > 0) {
     		return false;
     	}
-    	
+
     	return true;
     }
     
